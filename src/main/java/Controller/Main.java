ca724a5c95c3bdd2e9a3adf956976324930148f2
Zephyr <frastxini@gmail.com>
2025-08-10 16:10:42 +0300

    Solution

--- src/main/java/Controller/Main.java
+++ src/main/java/Controller/Main.java
@@ -1,9 +1,19 @@
 package Controller;
 import Distance.EditDistance;
 
+import java.util.Scanner;
+
 public class Main {
     static EditDistance editDistance = new EditDistance();
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first word: ");
+        String word1 = input.nextLine();
+        System.out.print("Enter second word: ");
+        String word2 = input.nextLine();
+
+        System.out.print("Min distance is: " + editDistance.minDistance(word1, word2));
     }
 }
